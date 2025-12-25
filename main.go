958faa2d10d1f358ada04d6fb4f4176b5bf3de98
Bernadette Angelice Novena Octaviani <belajarcodingangelicenovenaoct@gmail.com>
2025-12-25 14:27:04 +0700

    Create main.go

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+    // Angelice's First Go Program
+    fmt.Println("Hello, World! This is my Go-Core repository.")
+    
+    name := "Angel"
+    fmt.Printf("Developer: %s\n", name)
+    fmt.Println("Status: Learning Go for Rank S Achievement")
+}
